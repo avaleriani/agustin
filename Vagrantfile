6ad106233d06232f63e1bb849eba71529d1348d6
agustin <snoopy89@gmail.com>
2016-01-06 22:32:06 -0300

    -testing git with phpstorm
    -implemented grid not working

--- Vagrantfile
+++ Vagrantfile
@@ -2,13 +2,20 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-
     config.vm.box = "scotch/box"
     config.vm.network "private_network", ip: "192.168.33.10"
-    config.vm.hostname = "scotchbox"
+    config.vm.hostname = "scotchbox2"
     config.vm.synced_folder ".", "/var/www", :mount_options => ["dmode=777", "fmode=666"]
     
     # Optional NFS. Make sure to remove other synced_folder line too
     #config.vm.synced_folder ".", "/var/www", :nfs => { :mount_options => ["dmode=777","fmode=666"] }
-
+	config.push.define "ftp" do |push|
+		push.host = "185.27.134.11:21"
+		push.username = "kaboo_16923981"
+		push.password = "czjmdq0x"
+        push.destination = "/htdocs/test"
+        push.dir = "public"
+		push.secure = "false"
+	end
 end
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,145 +1,153 @@
-(function() {
-  var formSubscribe, loadApplication, svgInjector, worksFilter;
+(function () {
+    var formSubscribe, loadApplication, svgInjector, worksFilter;
 
-  loadApplication = function() {
-    $(svgInjector);
-    $(worksFilter);
-    $(formSubscribe);
-    $('.arrow-down').on('click', function(e) {
-      e.preventDefault();
-      return $("html,body").animate({
-        scrollTop: $("#page-work").offset().top
-      });
-    });
-    if (Modernizr.touch) {
-      return FastClick.attach(document.body);
-    }
-  };
-
-  svgInjector = function() {
-    var mySVGsToInject;
-    mySVGsToInject = document.querySelectorAll('img.inject-me');
-    return SVGInjector(mySVGsToInject);
-  };
-
-  worksFilter = function() {
-    var $btns, $filter, $grid, $mobileFilterBtn, $sizer, $workItem;
-    $filter = $('#work-filter');
-    $grid = $('#work-grid');
-    $sizer = $grid.find('.shuffle__sizer');
-    $workItem = $('.work-item');
-    $mobileFilterBtn = $('.mobile-filter-select');
-    $mobileFilterBtn.on('click', function(e) {
-      e.preventDefault();
-      $filter.slideToggle();
-      return $(this).toggleClass('opened');
-    });
-    $(window).resize(function() {
-      if ($(window).width() > 768) {
-        if ($filter.is(':visible')) {
-
-        } else {
-          return $filter.slideDown();
+    loadApplication = function () {
+        $(svgInjector);
+        $(worksFilter);
+        $(formSubscribe);
+        $('.arrow-down').on('click', function (e) {
+            e.preventDefault();
+            return $("html,body").animate({
+                scrollTop: $("#page-work").offset().top
+            });
+        });
+        if (Modernizr.touch) {
+            return FastClick.attach(document.body);
         }
-      } else {
-        $filter.slideUp();
-        return $mobileFilterBtn.removeClass('opened');
-      }
-    });
-    $workItem.on('click', function() {
-      var url;
-      url = $(this).data('url');
-      return window.location.href = url;
-    });
-    $grid.shuffle({
-      itemSelector: $workItem,
-      sizer: $sizer
-    });
-    $btns = $filter.children();
-    return $btns.on('click', function(e) {
-      var $this, group, isActive;
-      e.preventDefault();
-      $this = $(this);
-      isActive = $this.hasClass('active');
-      if ($this.hasClass('active')) {
-        return false;
-      } else {
-        group = (isActive ? 'all' : $this.data('group'));
-        if (!isActive) {
-          $('#work-filter .active').removeClass('active');
-        }
-        $this.toggleClass('active');
-        return $grid.shuffle('shuffle', group);
-      }
-    });
-  };
-
-  formSubscribe = function() {
-    var form, formMessages, hasHtml5Validation;
-    form = $('#subscribe');
-    formMessages = $('.form-result');
-    hasHtml5Validation = function() {
-      return typeof document.createElement("input").checkValidity === "function";
     };
-    if (hasHtml5Validation()) {
-      return form.submit(function(e) {
-        var formData;
-        if (!this.checkValidity()) {
-          e.preventDefault();
-          $(this).addClass("invalid");
-          return $("#status").html("invalid");
-        } else {
-          $(this).removeClass("invalid");
-          e.preventDefault();
-          formData = $(form).serialize();
-          return $.ajax({
-            type: "POST",
-            url: $(form).attr("action"),
-            data: formData
-          }).done(function(response) {
-            $(formMessages).removeClass("error");
-            $(formMessages).addClass("success");
-            $(formMessages).text('You have successfully subscribed!');
-            return $("#email").val("");
-          }).fail(function(data) {
-            $(formMessages).removeClass("success");
-            $(formMessages).addClass("error");
-            if (data.responseText !== "") {
-              return $(formMessages).text(data.responseText);
-            } else {
-              return $(formMessages).text("Oops! An error occured please check your email address.");
-            }
-          });
-        }
-      });
-    }
-  };
 
-    $(document).ready(function() {
-        $.support.placeholder = (function(){
+    svgInjector = function () {
+        var mySVGsToInject;
+        mySVGsToInject = document.querySelectorAll('img.inject-me');
+        return SVGInjector(mySVGsToInject);
+    };
+
+    worksFilter = function () {
+        var $btns, $filter, $grid, $mobileFilterBtn, $sizer, $workItem;
+        $filter = $('#work-filter');
+        $grid = $('#work-grid');
+        $sizer = $grid.find('.shuffle__sizer');
+        $workItem = $('.work-item');
+        $mobileFilterBtn = $('.mobile-filter-select');
+        $mobileFilterBtn.on('click', function (e) {
+            e.preventDefault();
+            $filter.slideToggle();
+            return $(this).toggleClass('opened');
+        });
+        $(window).resize(function () {
+            if ($(window).width() > 768) {
+                if ($filter.is(':visible')) {
+
+                } else {
+                    return $filter.slideDown();
+                }
+            } else {
+                $filter.slideUp();
+                return $mobileFilterBtn.removeClass('opened');
+            }
+        });
+        $workItem.on('click', function () {
+            var url;
+            url = $(this).data('url');
+            return window.location.href = url;
+        });
+        $grid.shuffle({
+            itemSelector: $workItem,
+            sizer: $sizer
+        });
+        $btns = $filter.children();
+        return $btns.on('click', function (e) {
+            var $this, group, isActive;
+            e.preventDefault();
+            $this = $(this);
+            isActive = $this.hasClass('active');
+            if ($this.hasClass('active')) {
+                return false;
+            } else {
+                group = (isActive ? 'all' : $this.data('group'));
+                if (!isActive) {
+                    $('#work-filter .active').removeClass('active');
+                }
+                $this.toggleClass('active');
+                return $grid.shuffle('shuffle', group);
+            }
+        });
+    };
+
+    formSubscribe = function () {
+        var form, formMessages, hasHtml5Validation;
+        form = $('#subscribe');
+        formMessages = $('.form-result');
+        hasHtml5Validation = function () {
+            return typeof document.createElement("input").checkValidity === "function";
+        };
+        if (hasHtml5Validation()) {
+            return form.submit(function (e) {
+                var formData;
+                if (!this.checkValidity()) {
+                    e.preventDefault();
+                    $(this).addClass("invalid");
+                    return $("#status").html("invalid");
+                } else {
+                    $(this).removeClass("invalid");
+                    e.preventDefault();
+                    formData = $(form).serialize();
+                    return $.ajax({
+                        type: "POST",
+                        url: $(form).attr("action"),
+                        data: formData
+                    }).done(function (response) {
+                        $(formMessages).removeClass("error");
+                        $(formMessages).addClass("success");
+                        $(formMessages).text('You have successfully subscribed!');
+                        return $("#email").val("");
+                    }).fail(function (data) {
+                        $(formMessages).removeClass("success");
+                        $(formMessages).addClass("error");
+                        if (data.responseText !== "") {
+                            return $(formMessages).text(data.responseText);
+                        } else {
+                            return $(formMessages).text("Oops! An error occured please check your email address.");
+                        }
+                    });
+                }
+            });
+        }
+    };
+
+    $(document).ready(function () {
+        //If is touchscreen, the "+ More" button in works is always visible
+        {
+            if (true) {
+                $(".info").addClass("work-button-mobile");
+            } else {
+                $(".info").removeClass("work-button-mobile");
+            }
+        }
+        $.support.placeholder = (function () {
             var i = document.createElement('input');
             return 'placeholder' in i;
         })();
 
-        if($.support.placeholder) {
-            $('.form-label').each(function(){
+        if ($.support.placeholder) {
+            $('.form-label').each(function () {
                 $(this).addClass('js-hide-label');
             });
 
-            $('.form-group').find('input, textarea').on('keyup blur focus', function(e){
+            $('.form-group').find('input, textarea').on('keyup blur focus', function (e) {
 
                 var $this = $(this),
                     $parent = $this.parent().find("label");
 
                 if (e.type == 'keyup') {
-                    if( $this.val() == '' ) {
+                    if ($this.val() == '') {
                         $parent.addClass('js-hide-label');
                     } else {
                         $parent.removeClass('js-hide-label');
                     }
                 }
                 else if (e.type == 'blur') {
-                    if( $this.val() == '' ) {
+                    if ($this.val() == '') {
                         $parent.addClass('js-hide-label');
                     }
                     else {
@@ -147,7 +155,7 @@
                     }
                 }
                 else if (e.type == 'focus') {
-                    if( $this.val() !== '' ) {
+                    if ($this.val() !== '') {
                         $parent.removeClass('js-unhighlight-label');
                     }
                 }
@@ -155,7 +163,7 @@
         }
     });
 
-  $(loadApplication);
+    $(loadApplication);
 
 
 }).call(this);
