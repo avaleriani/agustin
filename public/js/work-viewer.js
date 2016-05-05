c928bca279fe0200ae1dd66d6686a5fc091459e9
AV <agvaleriani@gmail,com>
2016-05-05 04:40:23 -0300

    project view implemented ready for testing.

--- public/js/work-viewer.js
+++ public/js/work-viewer.js
@@ -1,64 +1,57 @@
 var workViewer = {
-    originalTitle: document.title,
-    container: document.documentElement,
-    popup: document.querySelector('.avgrund-popup'),
-    cover: document.querySelector('.avgrund-cover'),
+    overlay: document.querySelector('div.overlay'),
+    closeBttn: overlay.querySelector('button.overlay-close'),
+    transEndEventNames: {
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'msTransition': 'MSTransitionEnd',
+        'transition': 'transitionend'
+    },
+    transEndEventName: transEndEventNames[Modernizr.prefixed('transition')],
+    support: {transitions: Modernizr.csstransitions},
 
     init: function () {
-        var that = this;
+        var thath = this;
         window.avgrund = {
             activate: that.activate,
-            deactivate: that.deactivate,
-            disableBlur: that.disableBlur
+            deactivate: that.deactivate
         };
+
     },
 
     activate: function () {
         document.addEventListener('keyup', workViewer.deactivateEscape, false);
-        document.addEventListener('click', workViewer.deactivateOnClickOutside, false);
-
-        workViewer.addClass(workViewer.popup, 'no-transition');
-
-        setTimeout(function () {
-            workViewer.removeClass(workViewer.popup, 'no-transition');
-            workViewer.addClass(workViewer.container, 'avgrund-active');
-        }, 0);
-        //document.title =  TODO:: get titulo del proyecto y ponerlo aca.
+        this.overlay.classList.add('open');
     },
 
     deactivate: function () {
+        var that = this;
         document.removeEventListener('keyup', workViewer.deactivateEscape, false);
-        document.removeEventListener('click', workViewer.deactivateOnClickOutside, false);
 
-        workViewer.removeClass(workViewer.container, 'avgrund-active');
+        if (this.overlay.classList.contains('open')) {
 
-        document.querySelector('#avimg').innerHTML = '';
-        document.querySelector('#avtitle').innerHTML = '';
-        document.querySelector('#avtext').innerHTML = '';
-        document.title = workViewer.originalTitle;
-    },
-
-    disableBlur: function () {
-        workViewer.addClass(document.documentElement, 'no-blur');
-    },
-
-    addClass: function (element, name) {
-        element.className = element.className.replace(/\s+$/gi, '') + ' ' + name;
-    },
-
-    removeClass: function (element, name) {
-        element.className = element.className.replace(name, '');
+            this.overlay.classList.remove('open');
+            this.overlay.classList.add('close');
+            var onEndTransitionFn = function (ev) {
+                if (that.support.transitions) {
+                    if (ev.propertyName !== 'visibility') return;
+                    this.removeEventListener(that.transEndEventName, onEndTransitionFn);
+                }
+                that.overlay.classList.remove('close');
+            };
+            if (this.support.transitions) {
+                that.overlay.addEventListener(that.transEndEventName, onEndTransitionFn);
+            }
+            else {
+                onEndTransitionFn();
+            }
+        }
     },
 
     deactivateEscape: function (e) {
         var code = e.keyCode || e.which;
         if (code === 27) {
-            workViewer.deactivate();
-        }
-    },
-
-    deactivateOnClickOutside: function (e) {
-        if (e.target === workViewer.cover) {
             workViewer.deactivate();
         }
     }
