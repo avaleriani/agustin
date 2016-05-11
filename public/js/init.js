58a1b401323ada24fb6a4bd2c5347670b320e1c1
AV <agvaleriani@gmail,com>
2016-05-11 02:57:15 -0300

    scss working and being loaded by webpack.

--- public/js/init.js
+++ public/js/init.js
@@ -1,10 +1,10 @@
+var scss = require('../scss/main.scss');
 //var linesDrawer = require('../js/lines-drawer.js');
 var app = require('../js/application.js');
-var scrollAnimation = require('../js/scroll-animation.js'); //todo: hacer
+//var scrollAnimation = require('../js/scroll-animation.js'); //todo: hacer
 var workViewer = require('../js/work-viewer.js');
 var animate = require('../js/animate.js');
 var fastClick = require('fastclick');
-
 
 document.addEventListener("DOMContentLoaded", function() {
     fastClick.attach(document.body);
@@ -15,9 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
     if (window.innerWidth == '1920') { //todo:: fix mobile
         // TODO:: ver como hacer para que las lineas sean mobile, supongo que multiplicando por un numero magico que de 0 en la resolucion que lo
         // hice = window.innerWidth * 1920
-        //          linesDrawer.createAllLines('#lines-container');
+        //          linesDrawer.createAllLine
+        // s('#lines-container');
         //          linesDrawer.scrollAnimate();
-        //linesDrawer.mouseCoordenatesOnTitle();
+        //l     inesDrawer.mouseCoordenatesOnTitle();
     }
     app.worksFilter();
     animate.typingEffect();
