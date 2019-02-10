ca9cbe81ea58898276b965e43b4fa2da9459a4b0
Agu Valeriani <agvaleriani@gmail.com>
2019-02-10 16:00:08 +0100

    -adding everything.

---
+++ mail/config.php
@@ -0,0 +1,9 @@
+<?php
+$data['host'] = gethostbyname('smtp.gmail.com');
+$data['username'] = 'valerianiagustin@gmail.com';
+$data['password'] = 'windows98';
+$data['port'] = 587;
+$data['form'] = 'mailer@agustinvaleriani.com';
+$data['to'] = 'me@agustinvaleriani.com';
+$data['cc'] = 'agvaleriani@gmail.com';
+
