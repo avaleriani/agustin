9969dc80538ac6af54b1ab404ab3dc36f6293d2a
Agustin Valeriani <agustin.valeriani@namecheap.com>
2020-11-05 19:38:08 +0100

    -Added prettier to project. -Processed files with prettier. -Bump version. -Translated all todo comments.

--- src/js/application.js
+++ src/js/application.js
@@ -1,13 +1,13 @@
 const $ = require("jquery");
 const SVGInjector = require("svg-injector");
-const fastClick = require('fastclick');
-import Shuffle from 'shufflejs';
+const fastClick = require("fastclick");
+import Shuffle from "shufflejs";
 
 export const app = {
-  loadApplication: function() {
+  loadApplication: function () {
     const that = this;
-    const touch = !!('ontouchstart' in document.documentElement || navigator.msMaxTouchPoints > 0);
-    $('.view').on('click', function(e) {
+    const touch = !!("ontouchstart" in document.documentElement || navigator.msMaxTouchPoints > 0);
+    $(".view").on("click", function (e) {
       e.preventDefault();
       return false;
     });
@@ -15,46 +15,50 @@ export const app = {
       return fastClick.attach(document.body);
     }
     this.centerGrid();
-    window.addEventListener('resize', function() {
-      that.centerGrid();
-    }, true);
+    window.addEventListener(
+      "resize",
+      function () {
+        that.centerGrid();
+      },
+      true
+    );
   },
 
-  svgInjector: function() {
-    const mySVGsToInject = document.querySelectorAll('img.inject-me');
+  svgInjector: function () {
+    const mySVGsToInject = document.querySelectorAll("img.inject-me");
     return SVGInjector(mySVGsToInject);
   },
 
-  worksFilter: function() {
-    const grid = document.getElementById('work-grid');
-    const sizer = document.getElementsByClassName('shuffle__sizer')[0];
-    const btns = document.getElementsByClassName('svg-wrapper');
+  worksFilter: function () {
+    const grid = document.getElementById("work-grid");
+    const sizer = document.getElementsByClassName("shuffle__sizer")[0];
+    const btns = document.getElementsByClassName("svg-wrapper");
     const shfl = new Shuffle(grid, {
-      itemSelector: '.work-item',
-      easing: 'ease-in-out',
+      itemSelector: ".work-item",
+      easing: "ease-in-out",
       sizer: sizer,
-      speed: 600
+      speed: 600,
     });
 
     Array.from(btns).forEach((btn) => {
       btn.addEventListener("click", (e) => {
         let group, isActive;
         e.preventDefault();
-        isActive = btn.classList.contains('active');
+        isActive = btn.classList.contains("active");
 
         if (isActive) {
           return false;
         } else {
-          group = btn.getElementsByTagName('a')[0].getAttribute('data-group');
-          $('.active').removeClass('active');
-          btn.classList.toggle('active');
+          group = btn.getElementsByTagName("a")[0].getAttribute("data-group");
+          $(".active").removeClass("active");
+          btn.classList.toggle("active");
           shfl.filter(group);
         }
       });
-    })
+    });
   },
 
-  centerGrid: function() {
+  centerGrid: function () {
     const width = window.innerWidth;
     let count = 0;
     let margin = (count - 1) * 15;
@@ -62,9 +66,9 @@ export const app = {
     if (width < total) {
       count = Math.floor(width / 270);
       margin = (count - 1) * 15;
-      total = (count * 270) + margin;
+      total = count * 270 + margin;
     }
-    document.getElementById('work-grid').style.width = total + "px";
-    document.getElementById('grid-box-container').style.width = total + "px";
-  }
+    document.getElementById("work-grid").style.width = total + "px";
+    document.getElementById("grid-box-container").style.width = total + "px";
+  },
 };
--- src/js/work-viewer.js
+++ src/js/work-viewer.js
@@ -1,43 +1,42 @@
 var workViewer = {
+  init: function () {
+    var elements = document.querySelectorAll(".work-item");
 
-    init: function () {
-        var elements = document.querySelectorAll('.work-item');
-
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].addEventListener('click', workViewer.activate, false);
-        }
-
-        var elem2 = document.querySelectorAll('.overlay-close');
-
-        for (i = 0; i < elem2.length; i++) {
-            elem2[i].addEventListener('click', workViewer.deactivate, false);
-        }
-    },
-
-    activate: function () {
-        var currElem = this.nextElementSibling;
-        if (!currElem.classList.contains('open')) {//if it's closed
-            document.addEventListener('keyup', workViewer.deactivateWithEscape, false);
-            currElem.classList.add('open');
-            currElem.querySelector(".overlay-title-text").classList.add('overlay-title-effect');
-        }
-    },
-
-    deactivate: function () {
-        var elem = document.querySelector('.open');
-        elem.classList.remove('open');
-        elem.querySelector(".overlay-title-text").classList.remove('overlay-title-effect');
-    },
-
-    deactivateWithEscape: function (e) {
-        var code = e.keyCode || e.which;
-        if (code === 27) {
-            var elem = document.querySelector(".open");
-            elem.classList.remove('open');
-            elem.querySelector(".overlay-title-text").classList.remove('overlay-title-effect');
-        }
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].addEventListener("click", workViewer.activate, false);
     }
-};
 
+    var elem2 = document.querySelectorAll(".overlay-close");
+
+    for (i = 0; i < elem2.length; i++) {
+      elem2[i].addEventListener("click", workViewer.deactivate, false);
+    }
+  },
+
+  activate: function () {
+    var currElem = this.nextElementSibling;
+    if (!currElem.classList.contains("open")) {
+      //if it's closed
+      document.addEventListener("keyup", workViewer.deactivateWithEscape, false);
+      currElem.classList.add("open");
+      currElem.querySelector(".overlay-title-text").classList.add("overlay-title-effect");
+    }
+  },
+
+  deactivate: function () {
+    var elem = document.querySelector(".open");
+    elem.classList.remove("open");
+    elem.querySelector(".overlay-title-text").classList.remove("overlay-title-effect");
+  },
+
+  deactivateWithEscape: function (e) {
+    var code = e.keyCode || e.which;
+    if (code === 27) {
+      var elem = document.querySelector(".open");
+      elem.classList.remove("open");
+      elem.querySelector(".overlay-title-text").classList.remove("overlay-title-effect");
+    }
+  },
+};
 
 module.exports = workViewer;
