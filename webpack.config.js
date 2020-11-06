6707dae8d2081b6fb10c57119c6f616ba54ebe05
Agustin Valeriani <11182223+avaleriani@users.noreply.github.com>
2020-11-06 14:34:25 +0100

    -Finished prettier inclusion.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const webpack = require('webpack');
-const path = require('path');
-const fs = require('fs');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const safePostCssParser = require('postcss-safe-parser');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const safePostCssParser = require("postcss-safe-parser");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const appDirectory = fs.realpathSync(process.cwd());
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
-module.exports = env => {
+module.exports = (env) => {
   const isProd = env === "production";
-  const cssFilename = 'static/css/[name].[contenthash:8].css';
-  const appBuild = resolveApp('build');
-  const appPublic = resolveApp('public');
-  const appHtml = resolveApp('public/index.html');
-  const appSrc = resolveApp('src');
+  const cssFilename = "static/css/[name].[contenthash:8].css";
+  const appBuild = resolveApp("build");
+  const appPublic = resolveApp("public");
+  const appHtml = resolveApp("public/index.html");
+  const appSrc = resolveApp("src");
   const cssRegex = /\.css$/;
   const cssModuleRegex = /\.module\.css$/;
   const sassRegex = /\.(scss|sass)$/;
@@ -25,76 +25,77 @@ module.exports = env => {
 
   const getStyleLoaders = (cssOptions, preProcessor) => {
     const loaders = [
-      !isProd && require.resolve('style-loader'),
+      !isProd && require.resolve("style-loader"),
       isProd && {
-        loader: MiniCssExtractPlugin.loader
+        loader: MiniCssExtractPlugin.loader,
       },
       {
-        loader: require.resolve('css-loader'),
-        options: cssOptions
+        loader: require.resolve("css-loader"),
+        options: cssOptions,
       },
       {
-        loader: require.resolve('postcss-loader'),
+        loader: require.resolve("postcss-loader"),
         options: {
-          ident: 'postcss',
+          ident: "postcss",
           plugins: () => [
-            require('postcss-flexbugs-fixes'),
-            require('postcss-preset-env')({
+            require("postcss-flexbugs-fixes"),
+            require("postcss-preset-env")({
               autoprefixer: {
-                flexbox: 'no-2009'
+                flexbox: "no-2009",
               },
-              stage: 3
-            })
+              stage: 3,
+            }),
           ],
-          sourceMap: isProd
-        }
-      }
+          sourceMap: isProd,
+        },
+      },
     ].filter(Boolean);
     if (preProcessor) {
       loaders.push({
         loader: require.resolve(preProcessor),
         options: {
-          sourceMap: isProd
-        }
+          sourceMap: isProd,
+        },
       });
     }
     return loaders;
   };
 
-
   return {
     bail: isProd,
     mode: env,
     performance: {
-      hints: !isProd ? false : "warning"
+      hints: !isProd ? false : "warning",
     },
-    devServer: !isProd ? {
-      contentBase: appPublic,
-      watchContentBase: true,
-      publicPath: '/',
-      hot: true,
-      inline: true,
-      quiet: false,
-      watchOptions: {
-        ignored: /node_modules/
-      },
-      https: true,
-      overlay: false,
-      clientLogLevel: 'none',
-      host: process.env.HOST || '0.0.0.0',
-      compress: true,
-      port: 3000,
-      historyApiFallback: true
-    } : {},
+    devServer: !isProd
+      ? {
+          contentBase: appPublic,
+          watchContentBase: true,
+          publicPath: "/",
+          hot: true,
+          inline: true,
+          quiet: false,
+          watchOptions: {
+            ignored: /node_modules/,
+          },
+          https: true,
+          overlay: false,
+          clientLogLevel: "none",
+          host: process.env.HOST || "0.0.0.0",
+          compress: true,
+          port: 3000,
+          historyApiFallback: true,
+        }
+      : {},
     entry: {
-      app: './src/index.js'
+      app: "./src/index.js",
     },
     output: {
       path: isProd ? path.resolve(__dirname, appBuild) : undefined,
-      filename: 'static/js/[name].[hash:8].js',
-      chunkFilename: 'static/js/[name].[hash:8].chunk.js',
+      filename: "static/js/[name].[hash:8].js",
+      chunkFilename: "static/js/[name].[hash:8].chunk.js",
       pathinfo: !isProd,
-      publicPath: '/'
+      publicPath: "/",
     },
     optimization: {
       minimize: isProd,
@@ -102,39 +103,39 @@ module.exports = env => {
         new TerserPlugin({
           terserOptions: {
             parse: {
-              ecma: 8
+              ecma: 8,
             },
             compress: {
               ecma: 5,
               warnings: false,
               comparisons: false,
-              inline: 2
+              inline: 2,
             },
             mangle: {
-              safari10: true
+              safari10: true,
             },
             output: {
               ecma: 5,
               comments: false,
-              ascii_only: true
-            }
+              ascii_only: true,
+            },
           },
           parallel: true,
           cache: true,
-          sourceMap: isProd
+          sourceMap: isProd,
         }),
         new OptimizeCSSAssetsPlugin({
           cssProcessorOptions: {
             parser: safePostCssParser,
             map: isProd
               ? {
-                inline: false,
-                annotation: true
-              }
-              : false
-          }
-        })
-      ]
+                  inline: false,
+                  annotation: true,
+                }
+              : false,
+          },
+        }),
+      ],
     },
     module: {
       strictExportPresence: true,
@@ -143,63 +144,63 @@ module.exports = env => {
           oneOf: [
             {
               test: [/\.bmp$/, /\.gif$/, /\.jpeg$/, /\.jpg$/, /\.png$/],
-              loader: require.resolve('url-loader'),
+              loader: require.resolve("url-loader"),
               options: {
                 limit: 10000,
-                name: 'static/images/[name].[hash:8].[ext]'
-              }
+                name: "static/images/[name].[hash:8].[ext]",
+              },
             },
             {
               test: /\.(js|mjs|jsx|ts|tsx)$/,
               include: appSrc,
-              loader: require.resolve('babel-loader'),
+              loader: require.resolve("babel-loader"),
               options: {
                 plugins: [
                   [
-                    require.resolve('babel-plugin-named-asset-import'),
+                    require.resolve("babel-plugin-named-asset-import"),
                     {
                       loaderMap: {
                         svg: {
-                          ReactComponent: '@svgr/webpack?-svgo![path]'
-                        }
-                      }
-                    }
-                  ]
+                          ReactComponent: "@svgr/webpack?-svgo![path]",
+                        },
+                      },
+                    },
+                  ],
                 ],
                 cacheDirectory: true,
                 cacheCompression: isProd,
-                compact: isProd
-              }
+                compact: isProd,
+              },
             },
             {
               test: /\.(js|mjs)$/,
               exclude: /@babel(?:\/|\\{1,2})runtime/,
-              loader: require.resolve('babel-loader'),
+              loader: require.resolve("babel-loader"),
               options: {
                 babelrc: true,
                 configFile: false,
                 compact: false,
                 cacheDirectory: true,
                 cacheCompression: isProd,
-                sourceMaps: isProd
-              }
+                sourceMaps: isProd,
+              },
             },
             {
               test: cssRegex,
               exclude: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
-                sourceMap: isProd
+                sourceMap: isProd,
               }),
-              sideEffects: true
+              sideEffects: true,
             },
             {
               test: cssModuleRegex,
               use: getStyleLoaders({
                 importLoaders: 1,
                 sourceMap: isProd,
-                modules: true
-              })
+                modules: true,
+              }),
             },
             {
               test: sassRegex,
@@ -207,11 +208,11 @@ module.exports = env => {
               use: getStyleLoaders(
                 {
                   importLoaders: 2,
-                  sourceMap: isProd
+                  sourceMap: isProd,
                 },
-                'sass-loader'
+                "sass-loader"
               ),
-              sideEffects: true
+              sideEffects: true,
             },
             {
               test: sassModuleRegex,
@@ -219,58 +220,56 @@ module.exports = env => {
                 {
                   importLoaders: 2,
                   sourceMap: isProd,
-                  modules: true
+                  modules: true,
                 },
-                'sass-loader'
-              )
+                "sass-loader"
+              ),
             },
             {
-              loader: require.resolve('file-loader'),
+              loader: require.resolve("file-loader"),
               exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
               options: {
-                name: 'static/media/[name].[hash:8].[ext]'
-              }
-            }
-          ]
-        }
-      ]
+                name: "static/media/[name].[hash:8].[ext]",
+              },
+            },
+          ],
+        },
+      ],
     },
     plugins: [
       new webpack.DefinePlugin(env),
       new CleanWebpackPlugin([appBuild], {}),
-      new CopyWebpackPlugin([
-        { from: appPublic }
-      ]),
+      new CopyWebpackPlugin([{ from: appPublic }]),
       new HtmlWebpackPlugin(
         Object.assign(
           {},
           {
             inject: true,
-            template: appHtml
+            template: appHtml,
           },
           isProd
             ? {
-              minify: {
-                removeComments: false,
-                removeEmptyElements: false,
-                collapseWhitespace: true,
-                removeRedundantAttributes: true,
-                useShortDoctype: true,
-                removeEmptyAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                keepClosingSlash: true,
-                minifyJS: true,
-                minifyCSS: true,
-                minifyURLs: true
+                minify: {
+                  removeComments: false,
+                  removeEmptyElements: false,
+                  collapseWhitespace: true,
+                  removeRedundantAttributes: true,
+                  useShortDoctype: true,
+                  removeEmptyAttributes: true,
+                  removeStyleLinkTypeAttributes: true,
+                  keepClosingSlash: true,
+                  minifyJS: true,
+                  minifyCSS: true,
+                  minifyURLs: true,
+                },
               }
-            }
             : undefined
         )
       ),
       new MiniCssExtractPlugin({
         filename: cssFilename,
-        chunkFilename: 'static/css/[name].[hash:8].chunk.css'
-      })
-    ]
-  }
+        chunkFilename: "static/css/[name].[hash:8].chunk.css",
+      }),
+    ],
+  };
 };
