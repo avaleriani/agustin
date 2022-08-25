259b5f6e6138d4eac37dce9de32a4abc05dfdea8
Agu Valeriani <agvaleriani@gmail.com>
2022-08-25 20:10:12 +0200

    fix icons and probably svg.

--- webpack.config.js
+++ webpack.config.js
@@ -188,7 +188,7 @@ module.exports = (env) => {
               ),
             },
             {
-              test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+              test: /.(ttf|otf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
               type: "asset/resource",
             },
           ],
